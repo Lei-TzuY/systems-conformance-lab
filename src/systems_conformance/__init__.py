5e3f2ab630ffce02352ba1ac889eff77d12e6c25
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-03 04:15:01 +0800

    feat: export differential comparator API

--- src/systems_conformance/__init__.py
+++ src/systems_conformance/__init__.py
@@ -1,4 +1,11 @@
+from .comparator import ComparisonResult, compare_results
 from .model import ExecutionResult, StreamCapture
 from .runner import run_process
 
-__all__ = ["ExecutionResult", "StreamCapture", "run_process"]
+__all__ = [
+    "ComparisonResult",
+    "ExecutionResult",
+    "StreamCapture",
+    "compare_results",
+    "run_process",
+]
