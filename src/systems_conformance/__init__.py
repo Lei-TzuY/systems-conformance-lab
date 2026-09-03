080790725fae633f0ac72ef607569e2c6fcbf88d
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-03 09:15:16 +0800

    feat: add generic reducer interface
    * feat: add generic reducer interface
    
    * test: cover generic reducer invariants
    
    * feat: export reducer API
    
    * docs: describe generic reducer foundation
    
    * test: fix reducer preservation fixture
    
    * fix reducer import lint

--- src/systems_conformance/__init__.py
+++ src/systems_conformance/__init__.py
@@ -1,14 +1,17 @@
 from .comparator import ComparisonResult, compare_results
 from .failure import FailureSignature, failure_signature
 from .model import ExecutionResult, StreamCapture
+from .reducer import ReductionResult, reduce_case
 from .runner import run_process
 
 __all__ = [
     "ComparisonResult",
     "ExecutionResult",
     "FailureSignature",
+    "ReductionResult",
     "StreamCapture",
     "compare_results",
     "failure_signature",
+    "reduce_case",
     "run_process",
 ]
