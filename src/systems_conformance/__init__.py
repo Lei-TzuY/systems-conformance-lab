7fe7940c8b4c7a71d9b1ee416470a41862754fd5
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-05 21:15:21 +0800

    feat: export fuzz witness triage

--- src/systems_conformance/__init__.py
+++ src/systems_conformance/__init__.py
@@ -22,6 +22,7 @@ from .repro import LoadedReproBundle, ReproBundle, load_repro_bundle, write_repr
 from .repro_archive import export_repro_archive, import_repro_archive
 from .retention import RetentionResult, enforce_repro_retention
 from .runner import run_process
+from .triage import ReducedFailureRepro, reduce_failure_to_repro
 from .write_fault import FaultingBinaryWriter
 
 __all__ = [
@@ -41,6 +42,7 @@ __all__ = [
     "FuzzDiscoveryResult",
     "FuzzFailure",
     "LoadedReproBundle",
+    "ReducedFailureRepro",
     "ReductionResult",
     "ReproBundle",
     "ReproReplay",
@@ -54,6 +56,7 @@ __all__ = [
     "import_repro_archive",
     "load_repro_bundle",
     "reduce_case",
+    "reduce_failure_to_repro",
     "run_failure_discovery_campaign",
     "run_feedback_guided_campaign",
     "run_fuzz_campaign",
