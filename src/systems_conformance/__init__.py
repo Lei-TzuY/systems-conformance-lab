c907746572724dff998e0c1640751f224b393eb2
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 02:17:47 +0800

    Fix package import ordering

--- src/systems_conformance/__init__.py
+++ src/systems_conformance/__init__.py
@@ -54,7 +54,6 @@ from .sqlite_query_triage import (
     reduce_sqlite_query_failure_to_repro,
 )
 from .sqlite_transaction_adapter import SQLiteTransactionTarget
-from .sqlite_two_connection_adapter import SQLiteTwoConnectionScenarioTarget
 from .sqlite_transaction_feedback import (
     SQLiteTransactionFeedbackEvaluator,
     sqlite_transaction_feedback_features,
@@ -72,6 +71,7 @@ from .sqlite_transaction_triage import (
     SQLiteTransactionReducedFailureRepro,
     reduce_sqlite_transaction_failure_to_repro,
 )
+from .sqlite_two_connection_adapter import SQLiteTwoConnectionScenarioTarget
 from .triage import ReducedFailureRepro, reduce_failure_to_repro
 from .write_fault import FaultingBinaryWriter
 
