3eb1815d6ebf0327418501ed6651b28acbbb4aed
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-23 09:05:04 +0800

    Export UTF-16 interoperability targets

--- src/systems_conformance/__init__.py
+++ src/systems_conformance/__init__.py
@@ -103,6 +103,8 @@ from .sqlite_two_connection_triage import (
 from .triage import ReducedFailureRepro, reduce_failure_to_repro
 from .unicode_normalization_adapter import UnicodeNormalizationTarget
 from .unicode_normalization_node_adapter import UnicodeNodeNormalizationTarget
+from .utf16_node_adapter import UTF16NodeDecodeTarget
+from .utf16_stream_adapter import UTF16DecodeTarget
 from .utf8_node_adapter import UTF8NodeDecodeTarget
 from .utf8_stream_adapter import UTF8DecodeTarget
 from .write_fault import FaultingBinaryWriter
@@ -154,6 +156,8 @@ __all__ = [
     "SQLiteTwoConnectionScenarioMutations",
     "SQLiteTwoConnectionScenarioTarget",
     "StreamCapture",
+    "UTF16DecodeTarget",
+    "UTF16NodeDecodeTarget",
     "UTF8DecodeTarget",
     "UTF8NodeDecodeTarget",
     "UnicodeNodeNormalizationTarget",
